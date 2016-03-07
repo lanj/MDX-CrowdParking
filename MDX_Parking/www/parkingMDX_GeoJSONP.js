f0c666d4f0632057caa8a7bcde3b61b43ccf0028
Lanre Jinadu <jinastic@googlemail.com>
2016-03-07 18:46:18 +0000

    modified files geojsonp file added, mulit-marker map added, click event label removed

--- MDX_Parking/www/parkingMDX_GeoJSONP.js
+++ MDX_Parking/www/parkingMDX_GeoJSONP.js
@@ -4,14 +4,39 @@ eqfeed_callback({
 "type": "FeatureCollection",
 "features": [
 	{"type": "Feature",
-		"geometry":{ "type": "Point", "coordinates": [51.5898860, -0.2259573 ]},
+		"geometry":{ "type": "Point", "coordinates": [-0.2259573, 51.5898860 ]},
 		"properties": { "street": "church street"}
 	},
     {"type": "Feature",
-		"geometry" :{"type": "Point", "coordinates": [51.5880536, -0.2288136]},
+		"geometry" :{"type": "Point", "coordinates": [-0.2286036 , 51.5880136 ]},
 		"properties": {"street": "edgerton street"}
-	}
+	},
+{"type": "Feature",
+	"geometry" :{"type": "Point", "coordinates": [-0.2287016 , 51.5879706 ]},
+	"properties": {"street": "edgerton street"}
+},
+{"type": "Feature",
+	"geometry":{ "type": "Point", "coordinates": [-0.2289265, 51.5880674 ]},
+	"properties": { "street": "st josephs grove"}
+},
+{"type": "Feature",
+	"geometry":{ "type": "Point", "coordinates": [-0.2289985, 51.5880644 ]},
+	"properties": { "street": "st josephs grove"}
+},
+{"type": "Feature",
+	"geometry" :{"type": "Point", "coordinates": [-0.2308136 , 51.5880407 ]},
+	"properties": {"street": "st josephs grove"}
+},
+{"type": "Feature",
+	"geometry":{ "type": "Point", "coordinates": [-0.2288047, 51.5911034 ]},
+	"properties": { "street": "church end"}
+},
+{"type": "Feature",
+	"geometry" :{"type": "Point", "coordinates": [-0.2280006 , 51.5912005 ]},
+	"properties": {"street": "church end"}
+}
+
 ]
 
-});
+})
 
