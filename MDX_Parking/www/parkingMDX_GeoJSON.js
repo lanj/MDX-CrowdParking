29ea877f5c8fb647742214a26ec91c161c584e18
Lanre Jinadu <jinastic@googlemail.com>
2016-03-07 14:20:49 +0000

    adding geojson data

---
+++ MDX_Parking/www/parkingMDX_GeoJSON.js
@@ -0,0 +1,17 @@
+
+
+{
+"type": "FeatureCollection",
+"features": [
+	{"type": "Feature",
+		"geometry":{ "type": "Point", "coordinates": [51.5898860, -0.2259573 ]},
+		"properties": { "street": "church street"}
+	},
+    {"type": "Feature",
+		"geometry" :{"type": "Point", "coordinates": [51.5880536, -0.2288136]},
+		"properties": {"street": "edgerton street"}
+	}
+]
+
+};
+
